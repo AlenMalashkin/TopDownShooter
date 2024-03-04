2b1f2dd9d164d5ebc478575156c41ab612554d15
Nikolay Klitovchenko <148769228+pochemur@users.noreply.github.com>
2024-03-04 16:12:38 +0300

    Исправление кода
    Пофиксил код в соответствии с ревью

--- Assets/Code/GameplayLogic/PlayerMovement.cs
+++ Assets/Code/GameplayLogic/PlayerMovement.cs
@@ -1,46 +1,30 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Code.GameplayLogic
 {
     [RequireComponent(typeof(CharacterController))]
     public class PlayerMovement : MonoBehaviour
+
     {
         [SerializeField] private float _speed;
 
         private Vector3 _moveDirection;
         private CharacterController _controller;
-        private bool _isMoving;
-        
-
-        private float CurrentMoveSpeed
-        {
-            get
-            {
-                if (_isMoving)
-                {
-                    return _speed;
-                }
-
-                return 0;
-            }
-        }
-
-        public void OnMove(InputAction.CallbackContext context)
-        {
-            _moveDirection = context.ReadValue<Vector3>();
-            _isMoving = _moveDirection != Vector3.zero;
-        }
 
         private void Update()
         {
-            _controller.Move(_moveDirection * CurrentMoveSpeed * Time.deltaTime);
+            _controller.Move(_moveDirection * _speed * Time.deltaTime);
         }
 
         private void OnValidate()
         {
             _controller ??= GetComponent<CharacterController>();
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            _moveDirection = context.ReadValue<Vector3>();
         }
     }
 }
