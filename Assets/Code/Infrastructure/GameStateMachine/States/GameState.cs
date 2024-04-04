8864e58cfb4bda77d23923852145965ad30e7cff
Nikolay Klitovchenko <148769228+pochemur@users.noreply.github.com>
2024-04-04 21:34:01 +0300

    Убрал ненужный код из ГеймСтейта

--- Assets/Code/Infrastructure/GameStateMachine/States/GameState.cs
+++ Assets/Code/Infrastructure/GameStateMachine/States/GameState.cs
@@ -105,21 +105,7 @@ namespace Code.Infrastructure.GameStateMachineNamespace.States
             return player;
         }
 
-
-        private void InitializeEnemy(Transform playerTransform)
-        {
-            GameObject enemy = _gameFactory.CreateEnemy(_levelStaticData
-                .EnemySpanwers[Random.Range(0, _levelStaticData.EnemySpanwers.Count)]);
-            enemy.GetComponent<EnemyMovement>()
-                .Init(playerTransform);
-        }
-
-        private void InitializeRangeEnemy(Transform playerTransform)
-        {
-            GameObject rangeEnemy = _gameFactory.CreateRangeEnemy(_levelStaticData
-                .EnemySpanwers[Random.Range(0, _levelStaticData.EnemySpanwers.Count)]);
-            rangeEnemy.GetComponent<RangeEnemyMovement>().Init(playerTransform);
-        }
+        
 
         private void InitializeHealthBar(Damageable damageable)
         {
